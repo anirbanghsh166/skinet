ec050063dfa7af7b90a0d5baeaacd80808641633
Anirban Ghosh <anirbanghsh166@gmail.com>
2023-02-19 14:32:14 +0530

    End of section 9

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -44,7 +44,7 @@ namespace Core.Specifications
 
         protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
         {
-            OrderBy = orderByDescExpression;
+            OrderByDescending = orderByDescExpression;
         }
 
         protected void ApplyPaging(int skip, int take)
